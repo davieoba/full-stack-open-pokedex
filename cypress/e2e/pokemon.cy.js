b2949f3d4172f67acf09bc79469de3eaa47ff044
davieoba <bodunrindavidbond@gmail.com>
2023-07-11 00:56:07 +0100

    feat: move my project to production

--- cypress/e2e/pokemon.cy.js
+++ cypress/e2e/pokemon.cy.js
@@ -1,5 +1,18 @@
 describe('Pokedex', () => {
   it('loads the front page', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('navigates to another page', () => {
+    cy.visit('http://localhost:5000/pokemon/ivysaur')
+  })
+})
+
+/**
+ * describe('Pokedex', () => {
+  it('loads the front page', () => {
     cy.visit('http://localhost:8080')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
@@ -9,3 +22,4 @@ describe('Pokedex', () => {
     cy.visit('http://localhost:8080/pokemon/ivysaur')
   })
 })
+ */
