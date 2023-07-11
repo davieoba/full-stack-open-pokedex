52fe969e526cbf4618437b4bf1419d4e7bffdf25
davieoba <bodunrindavidbond@gmail.com>
2023-07-12 00:06:36 +0100

    update

---
+++ health_check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+chmod +x ./health_check.sh
+echo "Hello from shell script"
+
+npm install curl
+
+# make GET request to '/health' endpoint
+response=$(curl -s http://localhost:5000/health)
+
+if["$response" == "ok"];then
+  echo "Health check passed: $response"
+  exit 0
+else
+  echo "Health check failed: $response"
+  exit 1 # exit status 1 means that the script "fails"
