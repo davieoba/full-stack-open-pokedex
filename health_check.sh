e058f506ffa9503ea6cf96048ae655161642e654
davieoba <bodunrindavidbond@gmail.com>
2023-07-12 01:20:16 +0100

    update bash script

--- health_check.sh
+++ health_check.sh
@@ -5,7 +5,7 @@ echo "Hello from shell script"
 npm install curl
 
 # make GET request to '/health' endpoint
-# response=$(curl -s http://localhost:5000/health)
+# response=$(curl -s https://fullstack-open-pokedex-b9yx.onrender.com/health)
 
 # if["$response" == "ok"];then
 #   echo "Health check passed: $response"
@@ -14,15 +14,17 @@ npm install curl
 #   echo "Health check failed: $response"
 #   exit 1 # exit status 1 means that the script "fails"
 
+
+# https://fullstack-open-pokedex-b9yx.onrender.com/health
 #!/bin/bash
 
-response=$(curl -s -o /dev/null -w "%{http_code}" https://your-render-service-url/health)
+response=$(curl -s -o /dev/null -w "%{http_code}" https://fullstack-open-pokedex-b9yx.onrender.com/health)
 if [ "$response" -ne 200 ]; then
   curl -X POST \
     -H "Authorization: Bearer $RENDER_TOKEN" \
     -H "Content-Type: application/json" \
     -d '{
-      "branch": "main",
+      "branch": "master",
       "commit": "'"$GITHUB_SHA"'",
       "overrides": {
         "env": {
@@ -30,5 +32,5 @@ if [ "$response" -ne 200 ]; then
         }
       }
     }' \
-    https://api.render.com/v1/services/<your-render-service-id>/deploys
+    https://api.render.com/v1/services/$SERVICE_ID/deploys
 fi
